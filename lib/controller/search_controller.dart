33eee6ee23def54d9cdffde1bcf2b44aeebbb8b7
zen47175 <zen_47175@hotmail.com>
2023-04-06 06:31:58 +0700

    update

--- lib/controller/search_controller.dart
+++ lib/controller/search_controller.dart
@@ -2,14 +2,15 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const String googleApiKey = 'YOUR_API_KEY_HERE';
+const String googleApiKey = 'AIzaSyBl4p1pmTY1BUnEYqteKEeEtFTd-qJuuG4';
 
 class SearchController {
   Future<List> searchRestaurants(String query) async {
     final response = await http.get(Uri.parse(
         'https://maps.googleapis.com/maps/api/place/textsearch/json?query=$query&type=restaurant&key=$googleApiKey'));
-
+    print('Request URL: $response');
     if (response.statusCode == 200) {
+      print('Response Body: ${response.body}');
       return json.decode(response.body)['results'];
     } else {
       throw Exception('Failed to load restaurants');
